e89b31454a3b611109895d7f19455793ca9d2737
Amir Abushareb <yields@icloud.com>
2019-08-24 16:14:18 +0300

    tests: rework override

--- tests/test_exec.py
+++ tests/test_exec.py
@@ -2,9 +2,11 @@
 import go.exec as exec
 
 def test_goenv():
+  orig = exec.environ
   exec.environ = { 'PATH': '/usr/local/bin' }
   v = exec.goenv('/')
   assert v.get('PATH') == ':'.join([
     '/usr/local/bin',
     v.get('GOPATH'),
   ])
+  exec.environ = orig
