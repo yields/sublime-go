b5100b1958171df32d3243a90aac2cf789bd5b8f
Amir Abushareb <yields@icloud.com>
2019-08-25 17:35:51 +0300

    tests: bump goenv timeout

--- tests/test_exec.py
+++ tests/test_exec.py
@@ -7,7 +7,7 @@ def test_goenv():
   orig = exec.environ
   exec.environ = { 'PATH': '/usr/local/bin' }
   pkg = path.join(os.getcwd(), 'examples')
-  v = exec.goenv(pkg, timeout=5)
+  v = exec.goenv(pkg, timeout=10)
   assert v.get('PATH') == ':'.join([
     '/usr/local/bin',
     path.join(v.get('GOPATH'), 'bin'),
