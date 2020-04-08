e09758148696ae6ee4a1cf4bbe4a93c3dcf91ee8
Amir Abushareb <yields@icloud.com>
2020-04-08 21:51:33 +0300

    scripts/package: match PC zip structure

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,7 +5,7 @@ set -eu
 main(){
   zip Golang.sublime-package go/*.py
   zip -j Golang.sublime-package scripts/dev.py
-  zip -j Golang.sublime-package conf/*
+  zip Golang.sublime-package conf/*
 }
 
 main
